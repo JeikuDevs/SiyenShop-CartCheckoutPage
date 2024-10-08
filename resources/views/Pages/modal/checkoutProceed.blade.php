3b7f9f06186f7fd8720eebb7f0df5fda04160ee9
Jervy Jake Morales <jeikudevs@gmail.com>
2024-10-08 23:09:11 +0800

    Created Cart Page and modal for confirmation.

---
+++ resources/views/Pages/modal/checkoutProceed.blade.php
@@ -0,0 +1,18 @@
+
+
+<div class="modal fade  " id="ModalProceed" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered  ">
+    <div class="modal-content ">
+      <div class="modal-header border-0">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body d-flex justify-content-center">
+        <p class="fw-semibold fs-6">Proceed to Checkout?</p>
+      </div>
+      <div class="modal-footer border-0 ">
+        <button type="button" class="btn btn-outline-primary btn-md w-25" data-bs-dismiss="modal">No</button>
+        <button type="button" class="btn btn-primary btn-md w-25">Yes</button>
+      </div>
+    </div>
+  </div>
+</div>
